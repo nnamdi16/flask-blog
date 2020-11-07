cbff318e273a3708aa8635a30d5935f8f4385f13
Nnamdi <nnwabuokei@paga.com>
2020-11-07 22:41:21 +0100

    Added register and login methods for registration and login form implementation

--- flaskblog.py
+++ flaskblog.py
@@ -1,6 +1,9 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for,flash, redirect
+from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'df76f403f57125a1299890ed0ad7f19d'
 
 posts = [
     {
@@ -29,6 +32,26 @@ def hello():
 def about():
     return render_template('about.html',title='About')
 
+
+@app.route('/register',methods=['GET','POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!','success')
+        return redirect(url_for('hello'))
+    return render_template('register.html',title='Register',form = form)
+
+
+@app.route('/login' ,methods=['GET','POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':  
+            flash(f'You have been logged in! ','success')
+            return redirect(url_for('hello'))
+        else:
+            flash('Login Unsuccessful, Please check username and password', 'danger')
+    return render_template('login.html',title='Login',form = form)
 
 if __name__ == '__main__':
     app.run(debug=True)
