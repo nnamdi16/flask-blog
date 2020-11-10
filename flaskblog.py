d1424637ad673d5cfbf33b2068933c7c1faeb9e5
Nnamdi <nnwabuokei@paga.com>
2020-11-10 11:24:34 +0100

    configured the SQLAlchemy, created a User and Post model

--- flaskblog.py
+++ flaskblog.py
@@ -1,9 +1,39 @@
+from datetime import datetime
 from flask import Flask, render_template, url_for,flash, redirect
+from flask_sqlalchemy import SQLAlchemy
 from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'df76f403f57125a1299890ed0ad7f19d'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    """
+    docstring
+    """
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    image_file = db.Column(db.String(20), nullable=False, default ='default.jpg')
+    password = db.Column(db.String(60), nullable=False)
+
+    def __repr__(self):
+        return f"User('{self.username}','{self.email}','{self.image_file}')"
+
+class Post(db.Model):
+    """
+    docstring
+    """
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    dated_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    image_file = db.Column(db.String(20), nullable=False, default ='default.jpg')
+    password = db.Column(db.String(60), nullable=False)
+
+    def __repr__(self):
+        return f"User('{self.username}','{self.email}','{self.image_file}')"
 
 posts = [
     {
@@ -57,3 +87,7 @@ if __name__ == '__main__':
     app.run(debug=True)
 
 # To resolve python issues - https://stackoverflow.com/questions/14792605/python-flask-import-error/19470924
+# pip install flask-sqlalchemy
+# python -m pip freeze > requirements.txt
+#  pip install -r requirements.txt
+# pip3 install flask-sqlalchemy  https://stackoverflow.com/questions/57473720/how-to-fix-importerror-cannot-import-name-sqlalchemy
